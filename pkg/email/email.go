04af113028979043396ed3f8f2cd5524704c9492
Sjors Gielen <sjors@sjorsgielen.nl>
2025-09-28 20:14:30 +0200

    Switch from Sendgrid to SMTP e-mail sending.

--- pkg/email/email.go
+++ pkg/email/email.go
@@ -1,25 +1,20 @@
 package email
 
 import (
+	"bytes"
 	"context"
 	"fmt"
-	"os"
-
-	"github.com/sendgrid/sendgrid-go"
-	"github.com/sendgrid/sendgrid-go/helpers/mail"
+	"io"
+	"log/slog"
+	"time"
 )
 
 var (
-	from = mail.NewEmail("Vierkantle", "info@vierkantle.nl")
+	from = "Vierkantle <info@vierkantle.nl>"
 )
-
-func GetSendgridApiKey() string {
-	return os.Getenv("SENDGRID_API_KEY")
-}
 
 func SendLoginUrl(ctx context.Context, email string, url string) error {
 	subject := "Je Vierkantle-login"
-	to := mail.NewEmail(email, email)
 	plainTextContent := `
 Iemand, waarschijnlijk jij, heeft geprobeerd in te loggen op Vierkantle met dit e-mailadres.
 Als jij dat niet was, negeer dan vooral dit mailtje, dan gebeurt er niets.
@@ -29,8 +24,7 @@ Was jij het wel, gebruik dan de volgende URL om in te loggen:
 
 Veel plezier met Vierkantlen!
 `
-	message := mail.NewSingleEmail(from, subject, to, plainTextContent, "")
-	return sendMail(ctx, message)
+	return sendMail(ctx, email, subject, plainTextContent)
 }
 
 func SendNewBoardInQueue(ctx context.Context, author string, name string) error {
@@ -41,18 +35,43 @@ Er staat een nieuw bord in de wachtrij van ` + author + `! Naam is ` + name + `.
 Zie: https://vierkantle.nl/generator
 `
 
-	message := mail.NewSingleEmail(from, subject, from, plainTextContent, "")
-	return sendMail(ctx, message)
+	return sendMail(ctx, from, subject, plainTextContent)
 }
 
-func sendMail(ctx context.Context, message *mail.SGMailV3) error {
-	client := sendgrid.NewSendClient(GetSendgridApiKey())
-	response, err := client.SendWithContext(ctx, message)
-	if err != nil {
-		return fmt.Errorf("sending e-mail failed because of %w", err)
+func sendMail(ctx context.Context, to string, subject string, content string) error {
+	var buffer bytes.Buffer
+
+	if err := write(&buffer,
+		fmt.Sprintf("Subject: %s\r\n", subject),
+		fmt.Sprintf("From: %s\r\n", from),
+		fmt.Sprintf("To: %s\r\n", to),
+		fmt.Sprintf("Date: %s\r\n", time.Now().Format(time.RFC1123Z)),
+		"\r\n\r\n",
+		content); err != nil {
+		return err
 	}
-	if response.StatusCode >= 300 {
-		return fmt.Errorf("sending e-mail failed because of HTTP status %d %q", response.StatusCode, response.Body)
+
+	err := SendMail([]string{to}, buffer.Bytes())
+	if err != nil {
+		slog.ErrorContext(ctx, "An e-mail failed to be sent",
+			slog.String("email", to),
+			slog.String("subject", subject),
+			slog.Any("err", err))
+		return err
+	} else {
+		slog.InfoContext(ctx, "An e-mail was sent",
+			slog.String("email", to),
+			slog.String("subject", subject))
+		return nil
+	}
+}
+
+func write(w io.Writer, parts ...string) error {
+	for _, p := range parts {
+		_, err := w.Write([]byte(p))
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
